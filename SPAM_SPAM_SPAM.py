97d37171f6ac4020795e04c72600287773900821
Martin-Stiller <49054392+Martin-Stiller@users.noreply.github.com>
2019-03-29 07:27:38 +0100

    Add files via upload

---
+++ SPAM_SPAM_SPAM.py
@@ -0,0 +1,18 @@
+from random import shuffle
+
+liste=("amazing gorgeous vibrant blazing stunning biggest fastest tremendous fantastic phenomenal delightful \
+ambitious staggering  smarter massive incredible spectacular super excited cool magical revolutionary intuitive").upper().split()
+
+shuffle(liste)
+
+for x in range(5):
+    for n in range(2):
+        for i in range(4):
+            print("Spam ",end='')
+        print()
+        
+    ele1 = liste.pop()
+    ele2 = liste.pop()
+
+    print("{} SPAM , {} SPAM".format(ele1, ele2))
+    print()
---
+++ schachbrett_reis.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+summe = 0
+feldliste = []
+
+for felder in range(64):
+    reiskorn = 2**felder
+    feldliste.append(reiskorn)
+    summe = summe + reiskorn
+    print ("Feld {} = {:>30,} Reiskörner und damit insgesamt {:>30,} Reiskörner".format(felder+1,reiskorn,summe))
+
+gewicht = summe * 0.02 / 1000 / 1000
+print()
+print ("Wenn ein Reiskorn 0.02g wiegt, wiegen alle Reiskörner {:,.0f} Tonnen".format(gewicht))
+plt.plot(feldliste)
+plt.show()
