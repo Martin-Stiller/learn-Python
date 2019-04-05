38b9f061796c52dbb4a1bec4687f49f229e2ce2d
Martin Stiller <martin@Horny.fritz.box>
2019-04-05 18:02:28 +0200

    update

--- gradkelvin.py
+++ gradkelvin.py
@@ -1,24 +1,24 @@
 
 # Definiere eine Funktion get_temp()
 def get_temp():
-    # Beginne schliefe  
+    # Beginne schleife  
     while True:
         # Usereingabe
         C = input("bitte Temperatur in Celsius angeben: ")
-        # prüfe ob die eingabe eien Zahl(flieskomma) ist 
+        # prüfe ob die eingabe eien Zahl (flieskomma) ist 
         try:
             C = float(C)
             # wenn es eine zahl ist gib die Zahl zurück
             return C
-            # ist dei zahl keine Zahl also es erscheint
+            # ist die Zahl keine Zahl und es erscheint
             # der Fehler ValueError
         except ValueError:
             # gib diese Fehlermeldung aus
-            # und wiederhole die schleife
+            # und wiederhole die Schleife
             print ("das ist keine Gültige eingabe")
-# Definiere die umrechnung von Kelvin nach °C
-# das ergebinis ist die Variable "C" , "K" wird nur innerhalb der 
-# definition zum rechnen benutzt!
+# Definiere die Umrechnung von Kelvin nach °C
+# das Ergebinis ist die Variable "C" , "K" wird nur innerhalb der 
+# Definition zum rechnen benutzt!
 def convert_to_kelvin(C):
     # berechne das Ergebnis        
     K = C + 237.15
@@ -27,9 +27,9 @@ def convert_to_kelvin(C):
 
 # Programmeinstieg
 if __name__ == "__main__":
-    # definiere die Variable C mit der get_temp definition
+    # definiere die Variable C mit der get_temp Definition
     C = get_temp()
     # wenn def_temp C zurückgibt  verwandle die variable C
-    # in einen string , berechne C mit der definition convert_to_kelvin
+    # in einen string , berechne C mit der Definition convert_to_kelvin
     # und gib das dann aus
     print("die Temperatur in Kelvin beträgt: " + str(convert_to_kelvin(C)))      
